0de7cbeecfc8526fcfb03adfdce1bca38cf8de2b
puerile22 <puerile22@gmail.com>
2020-11-12 23:11:17 -0800

    Update directory structure

--- setup.py
+++ setup.py
@@ -12,6 +12,5 @@ setup(
     version=open('VERSION').read().strip(),
     author='TipTap',
     install_requires=['tornado'],
-    package_dir={'tt_async_requests': 'src'},
     packages=['tt_async_requests']
 )
